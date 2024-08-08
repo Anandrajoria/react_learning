8b3af49dbad98ac5b588de9cf827a7a858c30add
Anandrajoriya <anandrajoriya03@gmail.com>
2024-08-08 17:16:08 +0530

    vite app

---
+++ my-vite-app/src/App.jsx
@@ -0,0 +1,10 @@
+import Script from './ToggleLightDark/Script';
+import SideBar  from './Responsive side bar/SideBar';
+export default function App() {
+    return (
+        <>
+            {/* <Script/> */}
+            <SideBar/>
+        </>
+    );
+}
