6fc16e580e33d1609a53be287cc9914edae8b5f8
Anandrajoriya <anandrajoriya03@gmail.com>
2024-11-09 22:31:15 +0530

    currency converter

---
+++ reactHooks/src/customHook/CurrencyConverter.jsx
@@ -0,0 +1,10 @@
+import Input from "./input"
+function CurrencyConverter() {
+  return (
+    <>
+        <h1 className="text-3xl bg-orange-500 text-center">currency converter</h1>
+    </>
+  )
+}
+
+export default CurrencyConverter
