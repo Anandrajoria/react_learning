9a3ddc6011f6ba401d47c32777b570224ff52dec
Anandrajoriya <anandrajoriya03@gmail.com>
2024-11-24 16:39:18 +0530

    starting

--- Routing/src/App.jsx
+++ Routing/src/App.jsx
@@ -1,18 +1,12 @@
-import Home from "./home/Home";
+
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import About from "./home/About";
-import Contact from "./home/Contact";
+
+
 function App() {
-  return <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home/>}>
-      <Route path="/about" element={<About/>}/>
-      <Route path="/contact" element={<Contact/>}/>
-      <Route path="*" element={<div>oops not availabe</div>}/>
-      </Route>
-    </Routes>
-  </BrowserRouter>;
+  return (
+    <>
+    </>
+  );
 }
 
 export default App;
