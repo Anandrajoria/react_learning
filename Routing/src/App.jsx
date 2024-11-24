5c9c7a021c194129699980fb1fb9752b93e48095
Anandrajoriya <anandrajoriya03@gmail.com>
2024-11-24 21:16:46 +0530

    routing

--- Routing/src/App.jsx
+++ Routing/src/App.jsx
@@ -1,12 +1,25 @@
-
 import "./App.css";
-
+import About from "./Components/about/About";
+import Contact from "./Components/contact/Contact";
+import ErrorPage from "./Components/error/ErrorPage";
+import Footer from "./Components/footer/Footer";
+import Header from "./Components/header/Header";
+import Home from "./Components/Home/Home";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <>
-    </>
-  );
+  return <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />                 {/* default path*/}
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+      <Footer />
+    
+
+  </>;
 }
 
 export default App;
