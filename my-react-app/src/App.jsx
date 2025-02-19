58e428c4c7eaef43da78b4da7652af8854496725
Anandrajoriya <anandrajoriya03@gmail.com>
2025-02-19 21:22:54 +0530

    components

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,45 +1,62 @@
 import React from "react";
 
-const PizzaData=[
+const PizzaData = [
   {
-    name:'forcassio',
-    ingridents:'bread with italian olive oil',
-    price:10,
-    photoName:'pizzas/forcassico.zip',
-    soldOut:false
+    name: "forcassio",
+    ingridents: "bread with italian olive oil",
+    price: 10,
+    photoName: "pizzas/forcassico.zip",
+    soldOut: false,
   },
   {
-    name:'marrgrita',
-    ingridents:'bread with italian olive oil',
-    price:10,
-    photoName:'pizzas/forcassico.zip',
-    soldOut:false
+    name: "marrgrita",
+    ingridents: "bread with italian olive oil",
+    price: 10,
+    photoName: "pizzas/forcassico.zip",
+    soldOut: false,
   },
   {
-    name:'vaggeis',
-    ingridents:'bread with italian olive oil',
-    price:10,
-    photoName:'pizzas/forcassico.zip',
-    soldOut:false
+    name: "vaggeis",
+    ingridents: "bread with italian olive oil",
+    price: 10,
+    photoName: "pizzas/forcassico.zip",
+    soldOut: false,
   },
   {
-    name:'paneer pizza',
-    ingridents:'bread with italian olive oil',
-    price:10,
-    photoName:'pizzas/forcassico.zip',
-    soldOut:false
+    name: "paneer pizza",
+    ingridents: "bread with italian olive oil",
+    price: 10,
+    photoName: "pizzas/forcassico.zip",
+    soldOut: false,
   },
-]
-function Pizza(){
-  return <h1>Pizza species</h1>
+];
+
+function Pizza() {
+  return (
+    <>
+      <h1>Pizza species </h1>
+      <p>bread with italian olive oil</p>
+      <br />
+    </>
+  );
 }
 
 function App() {
   return (
     <>
-      <h1>hellow world</h1>
-      <Pizza/>
+      <Header/>
+      <Pizza />
+      <Pizza />
     </>
   );
 }
 export default App;
+
+
+function Header(){
+  return <h1>fast react pizza co.</h1>
+}
+function Menu(){}
+function Footer(){
+  
+}
