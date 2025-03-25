03ad26a7586ae93b0de3628af9dfa90b71d19c76
Anandrajoriya <anandrajoriya03@gmail.com>
2025-03-25 23:00:30 +0530

    react

--- reactjs/src/components/Counter.jsx
+++ reactjs/src/components/Counter.jsx
@@ -3,16 +3,30 @@ import { useState } from "react";
 function Counter() {
     const [Count,setCount]=useState(0);
 
+    const [increment,setIncrement]=useState(1);
+
     function handleClick(){
-        setCount(Count+1)
+        setCount(Count+increment)
     }
     function decrement(){
-        setCount(Count-1)
+        setCount(Count-increment)
+    }
+
+    function increaseIncrement(){
+      setIncrement(increment+1)
+    }
+
+    function decreaseIncrement(){
+      setIncrement(increment-1)
     }
   return <div>
     <h1>count value is: {Count}</h1>
     <button onClick={handleClick}>Increment</button>
     <button onClick={decrement}>Dncrement</button>
+
+    <h1>we are incrementing the value by:{increment}</h1>
+    <button onClick={increaseIncrement}>increase the increment</button>
+    <button onClick={decreaseIncrement}>decrease the increment</button>
 
   </div>;
 }
@@ -21,3 +35,4 @@ export default Counter;
 
 //in order to save the count value we create the state variable
 // for example multiple component can store multiple piece of state these are called state variable
+
